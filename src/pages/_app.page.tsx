60d576f23ca3b1b6281f78181099cdf2544cb8a0
tqkoh <shktmygs@gmail.com>
2023-06-17 19:28:29 +0900

    deploy test

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -4,7 +4,7 @@ import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ClerkProvider {...pageProps} publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+    <ClerkProvider {...pageProps} publishableKey="pk_test_ZGlyZWN0LXNhd2Zpc2gtMjMuY2xlcmsuYWNjb3VudHMuZGV2JA">
       <Component {...pageProps} />
     </ClerkProvider>
   )
