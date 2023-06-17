2f127a3e4c24bbd31b67ffdd3157ccc0440c28a0
Nzt3 <nzt3pro@gmail.com>
2023-06-17 20:01:17 +0900

    多分こういうところを実装すると良いのだと思う

---
+++ src/pages/api/tasks/[id].ts
@@ -0,0 +1,45 @@
+const mysql = require('mysql2');
+const ulid = require('ulid');
+
+
+//本番はhostを変える
+const connection = mysql.createConnection({
+  host: '127.0.0.1',
+  user: 'root',
+  password: 'root',
+  database: 'h23s_08'
+});
+
+
+export default function personHandler(req,res) {
+  if (req.method === 'GET') {
+    console.log('GET');
+    connection.query(
+      'SELECT * FROM tasks',
+      (error, results) => {
+        results.forEach(element => {
+          console.log(element);
+        });
+        console.log(results);
+        res.status(200).send(results);
+      }
+    );
+  }else if(req.method === 'POST'){
+    connection.query(
+      'INSERT INTO tasks(id,author_id,title) VALUES(?,?,?)',
+      [ulid.ulid(),req.body.username,req.body.taskname],
+      (error,results) => {
+        console.log(results);
+        res.status(200).send(results);
+      }
+    )
+  }else if(req.method === 'DELETE'){
+    connection.query(
+
+    )
+  }else if(req.method === 'PUT'){
+    connection.query(
+
+    )
+  }
+}
