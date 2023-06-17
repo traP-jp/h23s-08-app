b7752d1f842bca12dc9d4787ce65fb5283a8c902
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2023-06-18 08:54:49 +0900

    ✨ お魚さん追加

--- src/components/MainView/index.tsx
+++ src/components/MainView/index.tsx
@@ -1,10 +1,9 @@
-import {
-  Stage,
-  Container,
-  Text,
-} from '@pixi/react'
+import { Stage, Container, Text } from '@pixi/react'
 import * as PIXI from 'pixi.js'
 import { SwayAquaticPlants } from './SwayAquaticPlants'
+import { Aji } from './Aji'
+import { EiRight } from './EiRight'
+import { EiLeft } from './EiLeft'
 
 interface Props {
   width: number
@@ -18,15 +17,18 @@ export const MainView: React.FC<Props> = ({ width, height }) => {
       height={height}
       options={{ backgroundColor: 0x1b2668 }}
     >
-    <Container x={0} y={0}>
-      <SwayAquaticPlants />
-      <Text
-        text='Hello World!'
-        style={new PIXI.TextStyle({ fill: 0xedf5f7 })}
-        x={0}
-        y={0}
-      />
-    </Container>
+      <Container x={0} y={0}>
+        <SwayAquaticPlants />
+        <Aji scale={0.5} x={width} y={height * 0.5} baseVelocity={0.8} />
+        <EiRight scale={0.5} x={-width} y={height * 0.2} baseVelocity={0.4} />
+        <EiLeft scale={0.5} x={width} y={height * 0.8} baseVelocity={0.4} />
+        <Text
+          text='Hello World!'
+          style={new PIXI.TextStyle({ fill: 0xedf5f7 })}
+          x={0}
+          y={0}
+        />
+      </Container>
     </Stage>
   )
 }
