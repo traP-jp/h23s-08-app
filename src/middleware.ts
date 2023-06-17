bb03d5dfc93d0e29c4428be653d20104b6e9898e
tqkoh <shktmygs@gmail.com>
2023-06-17 18:35:21 +0900

    add whoami damedesu

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,10 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware();
+export default authMiddleware({
+  afterAuth(_auth, _req, _evt) {
+    console.log("ok")
+  },
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
