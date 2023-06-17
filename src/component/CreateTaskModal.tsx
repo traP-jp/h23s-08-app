e2be1742c244a49ed47f20b9e9e4094269076992
hayatroid <hayatroid@gmail.com>
2023-06-17 15:14:28 +0900

    デザイン（雑）

--- src/component/CreateTaskModal.tsx
+++ src/component/CreateTaskModal.tsx
@@ -10,41 +10,75 @@ import { useState } from 'react';
 
 
 const CreateTaskModal = () => {
-  const ButtonOpen = styled(Dialog.Trigger)`
-    border: 1px solid black;
+  const Button = styled.button`
+    background: gold;
+    color: #885f30;
+    font-weight: bold;
+    padding: 4px 8px;
+    border-radius: 4px;
   `
-  const ButtonClose = styled(Dialog.Close)`
+  const ButtonTrigger = styled.button`
     border: 1px solid black;
   `
   const Modal = styled(Dialog.Content)`
-    border: 1px solid black;
+    /* 板の部分 */
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-content: center;
+    gap: 8px;
+    position: relative;
+    width: 320px;
+    height: 200px;
+    background: #ba9162;
+
+    /* 棒の部分 */
+    &::after {
+      content: "";
+      position: absolute;
+      display: block;
+      width: 32px;
+      height: 320px;
+      top: -20px;
+      left: calc(50% - 16px);
+      z-index: -1;
+      background: #885f30;
+    }
   `
   const ModalTitle = styled(Dialog.Title)`
+    color: white;
     font-size: large;
     font-weight: bold;
   `
   const InputContent = styled.textarea`
-    border: 1px solid black;
+    background: white;
+    width: 90%;
+    border-radius: 4px;
+    padding: 4px;
   `
+  const Overlay = styled(Dialog.Overlay)`
+    background-color: rgba(0, 0, 0, .5);
+    position: fixed;
+    inset: 0;
+  `
+
 
   const [contentValue, setContentValue] = useState<string>("");
   
   return (
     <Dialog.Root>
-      <ButtonOpen asChild>
-        <button>開く</button>
-      </ButtonOpen>
+      <Dialog.Trigger asChild>
+        <ButtonTrigger>開く</ButtonTrigger>
+      </Dialog.Trigger>
       <Dialog.Portal>
-        <Dialog.Overlay />
+        <Overlay />
         <Modal>
           <ModalTitle>タスクを追加</ModalTitle>
-          <InputContent id="content" onBlur={(e) => setContentValue(e.target.value)} placeholder="ここにタスクを入力">{contentValue}</InputContent>
-          <ButtonClose asChild onClick={() => {alert(contentValue + "を追加します"); setContentValue("")}}>
-            <button>追加</button>
-          </ButtonClose>
-          <ButtonClose asChild>
-            <button>閉じる</button>
-          </ButtonClose>
+          <InputContent id="content" onChange={(e) => setContentValue(e.target.value)} value={contentValue} placeholder="ここにタスクを入力"></InputContent>
+          <Button onClick={() => {alert(contentValue + "を追加します"); setContentValue("")}}>追加</Button>
+          <Dialog.Close asChild>
+            <Button>閉じる</Button>
+          </Dialog.Close>
         </Modal>
       </Dialog.Portal>
     </Dialog.Root>
