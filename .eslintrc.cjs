e80682f948a9b3b8b27ea66b1e84023d5b37b585
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2023-06-17 09:51:38 +0900

    👕 lint と formatter の設定入れる

---
+++ .eslintrc.cjs
@@ -0,0 +1,43 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'next/core-web-vitals',
+    'prettier',
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+  },
+  root: true,
+  plugins: ['react'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+      },
+    ],
+    '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+    '@typescript-eslint/strict-boolean-expressions': ['error'],
+    '@typescript-eslint/no-floating-promises': ['error'],
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+  },
+  ignorePatterns: ['node_modules'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+}
