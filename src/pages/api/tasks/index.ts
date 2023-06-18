1d4bbe64cfe729bfabcd7c2bed2fa671e15a84d8
Nzt3 <nzt3pro@gmail.com>
2023-06-18 14:30:27 +0900

    connectionを環境変数からの読み込みに変更

--- src/pages/api/tasks/index.ts
+++ src/pages/api/tasks/index.ts
@@ -3,11 +3,12 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import {ulid} from 'ulid';
 
 const connection = createConnection({
-  host: '127.0.0.1',
-  user: 'root',
-  password: 'root',
-  database: 'h23s_08'
-});
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+})
 
 interface Task {
   id : string,
