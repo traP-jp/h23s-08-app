b232de0c1410a790100053bd8626765b1e74dcfa
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2023-06-18 16:47:10 +0900

    🎨 OGP と favicon を設定

--- src/pages/modal-test.page.tsx
+++
@@ -1,5 +0,0 @@
-import CreateTaskModal from "@/components/CreateTaskModal";
-
-export default function modal_test() {
-    return <CreateTaskModal isLogin={true} groupList={["グループ1", "グループ2", "グループ3"]} />
-}
