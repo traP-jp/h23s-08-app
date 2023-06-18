61af11099a3314836da421a47e57c3bc9a7f4b8e
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2023-06-18 16:08:25 +0900

    ♻️ components の path を揃える

--- src/pages/modal-test.page.tsx
+++ src/pages/modal-test.page.tsx
@@ -1,4 +1,4 @@
-import CreateTaskModal from "@/component/CreateTaskModal";
+import CreateTaskModal from "@/components/CreateTaskModal";
 
 export default function modal_test() {
     return <CreateTaskModal isLogin={true} groupList={["グループ1", "グループ2", "グループ3"]} />
