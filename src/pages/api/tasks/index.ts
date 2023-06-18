e8d87b028b46a95a4335241594cf04d93f757b04
Nzt3 <nzt3pro@gmail.com>
2023-06-18 12:40:39 +0900

    delete,putのSQLを書いた 動くかはわからない

--- src/pages/api/tasks/index.ts
+++ src/pages/api/tasks/index.ts
@@ -1,32 +1,55 @@
-const mysql = require('mysql2');
-const ulid = require('ulid');
+import {createConnection} from 'mysql2';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import {ulid} from 'ulid';
 
-//本番はhostを変える
-const connection = mysql.createConnection({
+const connection = createConnection({
   host: '127.0.0.1',
   user: 'root',
   password: 'root',
   database: 'h23s_08'
 });
 
+interface Task {
+  id : string,
+  author_id: string,
+  title : string,
+  created_at: string
+}
 
-export default function personHandler(req,res) {
-  if (req.method == 'GET') {
+export default function handler(req: NextApiRequest,res: NextApiResponse) {
+  if (req.method === 'GET') {
     console.log('GET');
-    connection.query(
+    connection.execute(
       'SELECT * FROM tasks',
       (error, results) => {
-        results.forEach(element => {
-          console.log(element);
-        });
         console.log(results);
         res.status(200).send(results);
       }
     );
-  }else if(req.method == 'POST'){
-    connection.query(
+  }else if(req.method === 'POST'){
+    console.log('POST');
+    connection.execute(
       'INSERT INTO tasks(id,author_id,title) VALUES(?,?,?)',
-      [ulid.ulid(),req.body.username,req.body.taskname],
+      [ulid(),req.body.username,req.body.taskname],
+      (error,results) => {
+        console.log(results);
+        res.status(200).send(results);
+      }
+    )
+  }else if(req.method === 'DELETE'){
+    const id=req.body.id;
+    connection.execute(
+      'DELETE FROM tasks WHERE id = ?',[id],
+      (error,results)=>{
+        console.log(results);
+        res.status(200).end();
+      }
+    )
+  }else if(req.method === 'PUT'){
+    const task:Task = req.body.task;
+    connection.execute(
+      "UPDATE tasks SET author_id = ? , title = ?  WHERE id = ?",
+      [task.author_id,task.title,task.id],
       (error,results) => {
         console.log(results);
         res.status(200).send(results);
