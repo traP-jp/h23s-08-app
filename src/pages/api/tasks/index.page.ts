52dedeb76992d85e337851b42173833fc37a5251
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2023-06-18 17:13:27 +0900

    ✨ bulk insert できるように

--- src/pages/api/tasks/index.page.ts
+++ src/pages/api/tasks/index.page.ts
@@ -1,6 +1,6 @@
-import {createConnection} from 'mysql2';
-import type { NextApiRequest, NextApiResponse } from 'next';
-import {ulid} from 'ulid';
+import { createConnection } from 'mysql2'
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { ulid } from 'ulid'
 
 const connection = createConnection({
   host: process.env.DB_HOST,
@@ -11,50 +11,61 @@ const connection = createConnection({
 })
 
 interface Task {
-  id : string,
-  author_id: string,
-  title : string,
+  id: string
+  author_id: string
+  title: string
   created_at: string
 }
 
-export default function handler(req: NextApiRequest,res: NextApiResponse) {
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'GET') {
-    console.log('GET');
-    connection.execute(
-      'SELECT * FROM tasks',
-      (error, results) => {
-        console.log(results);
-        res.status(200).send(results);
-      }
-    );
-  }else if(req.method === 'POST'){
-    console.log('POST');
-    const task:Task = req.body.task;
+    console.log('GET')
+    connection.execute('SELECT * FROM tasks', (error, results) => {
+      console.log(results)
+      res.status(200).send(results)
+    })
+  } else if (req.method === 'POST') {
+    console.log('POST')
+    const task: Task | Task[] = req.body.task
+    if (Array.isArray(task)) {
+      const insertPreserved = task.map(_ => '(?,?,?)').join(',')
+      connection.execute(
+        `INSERT INTO tasks(id,author_id,title) VALUES ${insertPreserved}`,
+        task.flatMap(t => [ulid(), t.author_id, t.title]),
+        (error, results) => {
+          console.log(results)
+          res.status(200).send(results)
+        }
+      )
+      return
+    }
+
     connection.execute(
       'INSERT INTO tasks(id,author_id,title) VALUES(?,?,?)',
-      [ulid(),task.author_id,task.title],
-      (error,results) => {
-        console.log(results);
-        res.status(200).send(results);
+      [ulid(), task.author_id, task.title],
+      (error, results) => {
+        console.log(results)
+        res.status(200).send(results)
       }
     )
-  }else if(req.method === 'DELETE'){
-    const id=req.body.id;
+  } else if (req.method === 'DELETE') {
+    const id = req.body.id
     connection.execute(
-      'DELETE FROM tasks WHERE id = ?',[id],
-      (error,results)=>{
-        console.log(results);
-        res.status(200).end();
+      'DELETE FROM tasks WHERE id = ?',
+      [id],
+      (error, results) => {
+        console.log(results)
+        res.status(200).end()
       }
     )
-  }else if(req.method === 'PUT'){
-    const task:Task = req.body.task;
+  } else if (req.method === 'PUT') {
+    const task: Task = req.body.task
     connection.execute(
-      "UPDATE tasks SET author_id = ? , title = ?  WHERE id = ?",
-      [task.author_id,task.title,task.id],
-      (error,results) => {
-        console.log(results);
-        res.status(200).send(results);
+      'UPDATE tasks SET author_id = ? , title = ?  WHERE id = ?',
+      [task.author_id, task.title, task.id],
+      (error, results) => {
+        console.log(results)
+        res.status(200).send(results)
       }
     )
   }
